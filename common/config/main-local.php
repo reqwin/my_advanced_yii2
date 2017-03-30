5e9ee2d68b203fb43caa47ce19beae4deeceeb2b
Alexey Salashchenko <s.alex92@mail.ru>
2017-03-30 09:04:37 +0300

    вытащил из игнора

---
+++ common/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2_database',
+            'username' => 'root',
+            'password' => '1',
+            'charset' => 'utf8',
+			//'tablePrefix' => 'myprefix_',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
